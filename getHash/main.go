91477d63a75e926bd84adb68868fdb1fed568366
Maxime Leriche <maxleriche.60@gmail.com>
2022-03-31 14:51:27 +0200

    [add] proxy de merde

--- getHash/main.go
+++ getHash/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"flag"
-	"fmt"
 
 	"github.com/gin-gonic/gin"
 
 	"M1/Network/API/controller"
-	"M1/Network/API/fileVerification"
 	_ "M1/Network/API/docs"
+	"M1/Network/API/fileVerification"
 
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -36,10 +35,10 @@ func api() {
 		}
 	}
 	authorized := website.Group("/", gin.BasicAuth(gin.Accounts{
-        "user1": "love",
-    }))
+		"user1": "love",
+	}))
 	authorized.Any("/index.html", c.Protected)
-	
+
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.Run(":8080")
 }
@@ -51,7 +50,7 @@ func checkData() {
 func main() {
 	apiFlag := flag.Bool("api", false, "a bool")
 	flag.Parse()
-	
+
 	if *apiFlag {
 		api()
 	} else {
